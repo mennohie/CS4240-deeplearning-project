f211f8c7130a12765571d325b258d80031bc496f
Surya Narayanan <suryanarayananms95@gmail.com>
2023-03-23 17:16:35 +0100

    More documentation

--- utilities.py
+++ utilities.py
@@ -3,6 +3,13 @@ import torch
 
 
 def calculate_j_u(model, Y_u_pred):
+    """Calculates the jacobian for the boundary condition w.r.t.
+        model parameters.
+    model
+        Pytorch neural network model
+    Y_u_pred
+        The predictions for the boundary condition points
+    """
     n_params = sum([np.prod(p.size()) for p in model.parameters()])
     n_u_batchsize = Y_u_pred.shape[0]
     J_u = torch.zeros(size=(n_params, n_u_batchsize))
@@ -22,6 +29,12 @@ def calculate_j_u(model, Y_u_pred):
 
 
 def calculate_j_r(model, residual):
+    """Calculates the jacobian of the residual w.r.t. model parameters.
+    model
+        Pytorch neural network model
+    residual
+        The prediction of the network for the residual.
+    """
     n_params = sum([np.prod(p.size()) for p in model.parameters()])
     n_r_batchsize = residual.shape[0]
     J_r = torch.zeros(size=(n_params, n_r_batchsize))
@@ -40,9 +53,15 @@ def calculate_j_r(model, residual):
 
 
 def calculate_eigenvalues_from_j(j1, j2):
+    """ Calculates the eigenvalues of the kernel matrix.
+    K = j1.T@j2,
+    make sure that j1 has the shape (n_model_parameters, batch_size)
+    returns the eigenvalues in descending order for a (batch_size1, batch_size2)
+    matrix
+    """
     j1_np = j1.detach().numpy()
     j2_np = j2.detach().numpy()
-    K_matrix = j1.T@j2
+    K_matrix = j1_np.T@j2_np
     eigs = np.linalg.eigvalsh(K_matrix)
     # Return eigenvalues in descenind order
     return eigs[::-1]
